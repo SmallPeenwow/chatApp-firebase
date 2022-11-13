2d0b39afac7d0227dd68a94344e758feda7ce96f
SmallPeenwow <bowdencamryn@gmail.com>
2022-11-13 11:56:49 +0200

    Sass Responsive Design with Mixin Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Home from './pages/Home';
 import Register from './pages/Register';
 import './style.scss';
 
 function App() {
-	return <Register />;
+	// return <Register />;
+	return <Home />; // For testing
 }
 
 export default App;
