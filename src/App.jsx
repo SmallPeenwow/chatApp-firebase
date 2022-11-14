5c47cf738f3dfeafc4a41c2a2ef3ba521d44b5a4
SmallPeenwow <bowdencamryn@gmail.com>
2022-11-14 10:25:39 +0200

    React Router Dom Protected Route Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Register from './pages/Register';
 import './style.scss';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
@@ -8,12 +8,25 @@ import { AuthContext } from './context/AuthContext';
 function App() {
 	const { currentUser } = useContext(AuthContext);
 
+	const ProtectedRoute = ({ children }) => {
+		if (!currentUser) {
+			return <Navigate to='/login' />;
+		}
+	};
+
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
 					<Route path='/'>
-						<Route index element={<Home />} />
+						<Route
+							index
+							element={
+								<ProtectedRoute>
+									<Home />
+								</ProtectedRoute>
+							}
+						/>
 						<Route path='login' element={<Login />} />
 						<Route path='register' element={<Register />} />
 					</Route>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,49 @@
 import React from 'react';
 
+const handleSubmit = async (e) => {
+	e.preventDefault();
+
+	const displayName = e.target[0].value;
+	const email = e.target[1].value;
+	const password = e.target[2].value;
+	const file = e.target[3].files[0];
+
+	try {
+		const res = await createUserWithEmailAndPassword(auth, email, password);
+
+		const storageRef = ref(storage, displayName);
+
+		const uploadTask = uploadBytesResumable(storageRef, file);
+
+		uploadTask.on('state_changed', (snapshot) => {
+			(error) => {
+				setError(true);
+			};
+
+			() => {
+				getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+					await updateProfile(res.user, {
+						displayName,
+						photoURL: downloadURL,
+					});
+
+					await setDoc(doc(db, 'users', res.user.uid), {
+						uid: res.user.uid,
+						displayName,
+						email,
+						photoURL: downloadURL,
+					});
+
+					await setDoc(doc(db, 'userChats', res.user.uid), {});
+					navigate('./');
+				});
+			};
+		});
+	} catch (error) {
+		setError(true);
+	}
+};
+
 const Login = () => {
 	return (
 		<div className='formContainer'>
